1f4c4d3cb272c9462eab6602564195a732375091
SidWeston <60944006+SidWeston@users.noreply.github.com>
2025-06-12 18:37:54 +0100

    import tool packages
    animancer, amplify shader editor, and odin inspector

---
+++ GJL Game Parade Spring 2025/Assets/Scripts/InputManager.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static PlayerInput;
+
+public class InputManager : MonoBehaviour, IPlayerActions
+{
+    public static InputManager instance;
+    private PlayerInput input;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
