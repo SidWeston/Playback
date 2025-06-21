f49e007dd5146bbf16ead41c00524863fd0578ea
SidWeston <60944006+SidWeston@users.noreply.github.com>
2025-06-21 21:07:57 +0100

    final fixes

--- GJL Game Parade Spring 2025/Assets/Scripts/UI/GameUI.cs
+++ GJL Game Parade Spring 2025/Assets/Scripts/UI/GameUI.cs
@@ -106,10 +106,6 @@ public class GameUI : MonoBehaviour
             {
                 OpenPauseMenu();
             }
-            else
-            {
-                ClosePauseMenu();
-            } 
         }
     }
 
