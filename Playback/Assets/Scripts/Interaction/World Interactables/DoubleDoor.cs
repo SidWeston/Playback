f305ab6042c413ac8fb2bc518b58be0d98bec17d
SidWeston <60944006+SidWeston@users.noreply.github.com>
2026-02-24 21:57:42 +0000

    1

---
+++ Playback/Assets/Scripts/Interaction/World Interactables/DoubleDoor.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class DoubleDoor : ActivatableObject
+{
+    [SerializeField] private List<Door> doors = new List<Door>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Activate(GameObject activator)
+    {        
+        Vector3 directionToPlayerFromDoor = activator.transform.position - transform.position;        
+        int direction = Vector3.Dot(directionToPlayerFromDoor, transform.forward) > 0.5f ? 1 : -1;
+        for (int i = 0; i < doors.Count; i++)
+        {            
+            doors[i].SetOpenDirection(direction);
+            doors[i].Activate(activator);
+        }
+    }
+
+    public override void Deactivate()
+    {
+        for (int i = 0; i < doors.Count; i++)
+        {
+            doors[i].Deactivate();
+        }
+    }
+}
